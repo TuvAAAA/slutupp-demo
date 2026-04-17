04f12a3ab32afb54b7b8fa98256380508fae9dfe
TuvAAAA <tuva.tenghall@elev.ga.ntig.se>
2026-04-17 09:37:19 +0200

    la till lite while loopar för att göra så att is satserna kunde leda till samma sak och få samma output, kommer göra de nästa lektion också. O lite mer story, ska försöka börja på listan för att kunna packa saker, få in main delarna alltså.

--- demo.py
+++ demo.py
@@ -16,15 +16,20 @@ while part_1:
     if choice == 1:
         input("AACK! You open the door and there seems to have been a massive flood during the night, luckly you live on a hill so it didn't take your house with you.")
         choice2 = int(input("what do you wanna do now?      Go back inside[1]       Figure out what happend[2]      "))
-        if choice2 ==1:
-            input("you go back inside but something in you wants to fin out why there is so much water(also you might die of starvation)")
+        while choice2 == 1:
+            input("You go back inside but something in you wants to find out why there is so much water(also you might die of starvation). Then you see that you have an unred text from your friend: There has been a massive water rise, we're all gonna gather at the town upp on the mountain.")
+            choice2 = choice2-1
 
-        if choice ==2:
+        while choice2 == 2:
             input("You decide to call some friends, nothing. You try the tv, nothing. And last you try the radio, only static noise. Then you see that you have an unred text from your friend: There has been a massive water rise, we're all gonna gather at the town upp on the mountain.")
-            
+            choice2 = choice2 -2
+
+        input("You think back to the old kayak you had gotten on impuls and decide och check if you still have it")
+    
+
 
     elif choice == 2:
-        input("nothing is out of the ordianry, your room is a bit messy tho.")
+        input("Nothing is out of the ordianry, your room is a bit messy tho.")
 
 
     elif choice == 3:
