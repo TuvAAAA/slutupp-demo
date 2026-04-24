d616a7fba32ef55d425ce9b96e0ee05b14028eae
TuvAAAA <tuva.tenghall@elev.ga.ntig.se>
2026-04-24 09:37:55 +0200

    bro what ens, kommer ty patt bli en liten wordle sak ev det, men nu ör jag klar med första strn och har börjat med att åka ifrån

--- demo.py
+++ demo.py
@@ -1,5 +1,13 @@
 
 
+from random import randint
+
+word_list= ["cordyceps"]
+lock = 5
+word = list(word_list[(0,len(word_list)- 1)])
+guessed_password = [""]*len(word)
+guessed_letters = []
+
 
 bag = []
 max_items = 20
@@ -28,10 +36,20 @@ while part_1:
 
         elif choice == 2:
             input("Nothing is out of the ordianry, your room is a bit messy tho.")
+            while choice ==2:
+                input("You notice your phone lighting up and see that you have a text message from you friend that reads: There has been a massive water rise, we're all gonna gather at the town upp on the mountain.")
+                input("you wonder how youd get there now that there have been a massive water rise.")
+                choice =-2
+                md1=md1-1
             #story
 
         elif choice == 3:
             input("You turn on the tv but its just static noise and a gray screen, seems like the signal isn't working.")
+            while choice ==3:
+                input("You notice a text message from your friend that reads: There has been a massive water rise, we're all gonna gather at the town upp on the mountain.")
+                input("you wonder how youd get there now that there have been a massive water rise.")
+                choice =-3
+                md1=md1-1
             #story
 
     
@@ -39,6 +57,8 @@ while part_1:
     input("You do! thankfully it's also in a good condition. Now the only thing left to do is pack some recorces to bring with you")
     bagrun = True
    #bag, lägg till saker mm
+
+    #göra så att man alltid har access till sin bag
     while bagrun:
         recorces = print("What do you wanna bring:         water, snacks, food, aidkit, patchkit, flashlight, batteries, emergency flare, ....")
         print("Add [A]")
@@ -64,7 +84,33 @@ while part_1:
         elif choice.lower()=="i":
             print(bag)
 
-    print("tjoho")
+    input("Great now you have some stuff on your journey")
+    input("You get into your kayak with your stuff and start to paddle towards the moutain")
+    way = int(input("The quote on quote road turns into three sections all leading to the moutain, which do you take:     Forest[1]       Open[2]     City[3]      "))
+    if way ==1:
+        input("You decide to take the forest path and admire how it looked even though this feel kinda scary")
+        input("it's really quiet, too quiet, before you see something move in the distance")
+
+    if way ==2:
+        w
+
+    if way ==3:
+        
+        input("You decide to go the city way to see the wreckage that the flood had caused")
+        input("You continue calm rowing and suddely you see something floating in the need distance, you go towards it, pick it up and see it was some sort of laptop.")
+        
+        
+
+        while lock>1:
+            input("you open it upp and somehow it starts upp and not being water damaged, it loads a little bit before it shows: ENTER PASSWORD     ")
+            input("it was a 9 letter combination, it couldn't be that hard right?")
+            letters = input("ENTER PASSWORD")
+            list(letters)
+            for l in letters:
+                
+
+
+
         
             
             
